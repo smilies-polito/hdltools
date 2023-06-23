57493d4938b64f24a40054649d389a65b6c3fe7e
alessio <alessio.carpegna@polito.it>
2023-06-23 17:11:14 +0200

    Make the adder a python class.
    Up to now it was hardcoded. Make it a callable class.

--- hdltools/adder.py
+++ hdltools/adder.py
@@ -1,18 +1,28 @@
 import vhdl_gen
 
-adder = vhdl_gen.BasicVHDL("adder", "behavior")
+class Adder(vhdl_gen.BasicVHDL):
 
-adder.library.add("ieee")
-adder.library["ieee"].package.add("std_logic_1164")
-adder.library["ieee"].package.add("numeric_std")
+	def __init__(self, entity_name, architecture_name, bit_width):
+		vhdl_gen.BasicVHDL.__init__(self, entity_name, architecture_name)
+		self.bit_width = bit_width
 
-adder.entity.generic.add("bit_width", "integer", "8")
+		self.library.add("ieee")
+		self.library["ieee"].package.add("std_logic_1164")
+		self.library["ieee"].package.add("numeric_std")
 
-adder.entity.port.add("in0", "in", "signed(bit_width-1 downto 0)")
-adder.entity.port.add("in1", "in", "signed(bit_width-1 downto 0)")
-adder.entity.port.add("add_out", "out", "signed(bit_width-1 downto 0)")
+		self.entity.generic.add("bit_width", "integer", self.bit_width)
+
+		self.entity.port.add("in0", "in", "signed(bit_width-1 downto 0)")
+		self.entity.port.add("in1", "in", "signed(bit_width-1 downto 0)")
+		self.entity.port.add("add_out", "out", "signed(bit_width-1 downto 0)")
 
 
-adder.architecture.bodyCodeHeader.add("add_out <= in0 + in1;")
+		self.architecture.bodyCodeHeader.add("add_out <= in0 + in1;")
 
-adder.write_file()
+		self.write_file()
+
+adder = Adder(
+	entity_name 		= "adder",
+	architecture_name 	= "behavior",
+	bit_width		= 10
+)
