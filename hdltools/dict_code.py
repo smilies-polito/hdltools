331325f6d9364b6f1d24dd0b46c4a836f37b7d84
alessio <alessio.carpegna@polito.it>
2023-07-04 15:39:47 +0200

    Documented and tested text gen from dictionary

--- hdltools/dict_code.py
+++ hdltools/dict_code.py
@@ -1,23 +1,70 @@
 from format_text import indent
 
-def VHDLenum(list, indent_level = 0):
+def VHDLenum_dict(objects_dict : dict, indent_level : int = 0) -> str:
+
+	"""
+	Generate a string concatenating all the elements of a dictionary of
+	objects, removing the last terminating character (; or ,).
+
+	Parameters:
+	-----------
+	objects_dict	: dictionary of objects.
+		The object must implement the code() method.
+	indent_level	: int, optional
+		Number of indentations to insert between printed elements.
+
+	Return:
+	-------
+	hdl_code	: str
+		Generated string
+	"""
+
 	hdl_code = ""
+
 	i = 0
-	for j in list:
+
+	for key in objects_dict:
 		i = i+1
-		if (i == len(list)):
-			tmp = list[j].code()
+		if (i == len(objects_dict)):
+
+			# Remove terminating character
+			tmp = objects_dict[key].code()
 			tmp = tmp.replace(";", "")
 			tmp = tmp.replace(",", "")
+
 			hdl_code = hdl_code + indent(indent_level) + tmp
+
 		else:
 			hdl_code = hdl_code + indent(indent_level) + \
-					list[j].code()
+					objects_dict[key].code()
 	return hdl_code
 
 
-def DictCode(DictInput, indent_level = 0):
-    hdl_code = ""
-    for j in DictInput:
-        hdl_code = hdl_code + indent(indent_level) + DictInput[j].code()
-    return hdl_code
+
+
+def DictCode(objects_dict : dict, indent_level : int = 0) -> str:
+
+	"""
+	Generate a string concatenating all the elements of a dictionary of
+	objects.
+
+	Parameters:
+	-----------
+	objects_dict	: dictionary of objects.
+		The object must implement the code() method.
+	indent_level	: int, optional
+		Number of indentations to insert between printed elements.
+
+	Return:
+	-------
+	hdl_code	: str
+		Generated string
+	"""
+
+	hdl_code = ""
+
+	for key in objects_dict:
+		hdl_code = hdl_code + indent(indent_level) + \
+				objects_dict[key].code()
+	
+	return hdl_code
