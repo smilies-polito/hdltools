6639c7550569b4e1e7a3b7a8514be35a25629dc1
alessio <alessio.carpegna@polito.it>
2023-07-06 14:50:18 +0200

    COrrect indentation entity

--- hdltools/entity.py
+++ hdltools/entity.py
@@ -1,3 +1,8 @@
+from format_text import indent
+from generic import GenericList
+from port import PortList
+
+
 class Entity:
 
 	def __init__(self, name):
@@ -5,30 +10,39 @@ class Entity:
 		self.generic = GenericList()
 		self.port = PortList()
 
-	def code(self, indent_level=0):
+	def code(self, indent_level = 0):
 
-		hdl_code = indent(0) + ("entity %s is\n" % self.name)
+		hdl_code = indent(indent_level) + ("entity %s is\n" % self.name)
 
 		if (self.generic):
-			hdl_code = hdl_code + indent(1) + ("generic (\n")
-			hdl_code = hdl_code + self.generic.code(2)
-			hdl_code = hdl_code + indent(1) + (");\n")
+			hdl_code = hdl_code + indent(indent_level+1) + \
+					("generic (\n")
+			hdl_code = hdl_code + self.generic.code(indent_level+2)
+			hdl_code = hdl_code + indent(indent_level+1) + (");\n")
 
 		else:
-			hdl_code = hdl_code + indent(1) + ("--generic (\n")
-			hdl_code = hdl_code + indent(2) + ("--generic_declaration_tag\n")
-			hdl_code = hdl_code + indent(1) + ("--);\n")
+			hdl_code = hdl_code + indent(indent_level+1) + \
+					("--generic (\n")
+			hdl_code = hdl_code + indent(indent_level+2) + \
+					("--generic_declaration_tag\n")
+			hdl_code = hdl_code + indent(indent_level+1) + \
+					("--);\n")
 
 		if (self.port):
-			hdl_code = hdl_code + indent(1) + ("port (\n")
-			hdl_code = hdl_code + self.port.code(2)
-			hdl_code = hdl_code + indent(1) + (");\n")
+			hdl_code = hdl_code + indent(indent_level+1) + \
+					("port (\n")
+			hdl_code = hdl_code + self.port.code(indent_level+2)
+			hdl_code = hdl_code + indent(indent_level+1) + (");\n")
 
 		else:
-			hdl_code = hdl_code + indent(1) + ("--port (\n")
-			hdl_code = hdl_code + indent(2) + ("--port_declaration_tag\n")
-			hdl_code = hdl_code + indent(1) + ("--);\n")
-			hdl_code = hdl_code + indent(0) + ("end %s;\n" % self.name)
+			hdl_code = hdl_code + indent(indent_level+1) + \
+					("--port (\n")
+			hdl_code = hdl_code + indent(indent_level+2) + \
+					("--port_declaration_tag\n")
+			hdl_code = hdl_code + indent(indent_level+1) + \
+					("--);\n")
+			hdl_code = hdl_code + indent(indent_level) + \
+					("end %s;\n" % self.name)
 			hdl_code = hdl_code + "\n"
 
 		return hdl_code
