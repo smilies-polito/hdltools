d592c3353f7b11b866826232c71c6e892aa2ae17
alessio <alessio.carpegna@polito.it>
2023-07-06 14:51:07 +0200

    Create process list

--- hdltools/process.py
+++ hdltools/process.py
@@ -1,5 +1,5 @@
 from text import SingleCodeLine, GenericCodeBlock
-from dict_code import VHDLenum
+from dict_code import VHDLenum, DictCode
 from format_text import indent
 from if_statement import IfList
 from for_statement import ForList
@@ -39,25 +39,40 @@ class Process:
 
 		hdl_code = ""
 
-		if(self.name == ""):
+		if self.body:
+			if(self.name == ""):
+				hdl_code = hdl_code + indent(indent_level) + \
+						"process(" + \
+						self.sensitivity_list.code() + \
+						")\n"
+
+			else:
+				hdl_code = hdl_code + indent(indent_level) + \
+						self.name + " : process(" + \
+						self.sensitivity_list.code() + \
+						")\n"
+
+			hdl_code = hdl_code + indent(indent_level) + "begin\n\n"
+
+			hdl_code = hdl_code + self.for_list.code(indent_level \
+						+ 1)
+			hdl_code = hdl_code + self.if_list.code(indent_level \
+						+ 1)
+			hdl_code = hdl_code + self.body.code(indent_level + 1)
+
 			hdl_code = hdl_code + indent(indent_level) + \
-					"process(" + \
-					self.sensitivity_list.code() + \
-					")\n"
-
-		else:
-			hdl_code = hdl_code + indent(indent_level) + \
-					self.name + " : process(" + \
-					self.sensitivity_list.code() + \
-					")\n"
-
-		hdl_code = hdl_code + indent(indent_level) + "begin\n\n"
-
-		hdl_code = hdl_code + self.for_list.code(indent_level + 1)
-		hdl_code = hdl_code + self.if_list.code(indent_level + 1)
-		hdl_code = hdl_code + self.body.code(indent_level + 1)
-
-		hdl_code = hdl_code + indent(indent_level) + \
-				"\nend process " + self.name + ";\n\n"
+					"\nend process " + self.name + ";\n\n"
 
 		return hdl_code
+
+class ProcessList(dict):
+
+	def __init__(self):
+		self.index = 0
+
+	def add(self, name : str = "", *args):
+		self[self.index] = Process(name, *args)
+		self.index = self.index + 1
+
+	def code(self, indent_level : int = 0):
+		return DictCode(self, indent_level)
