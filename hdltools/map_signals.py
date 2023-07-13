1996b6ccb970d4a90739165c13d266b918dbdaf6
alessio <alessio.carpegna@polito.it>
2023-07-13 18:32:04 +0200

    Generalize port and generic map.
    Allow to connect signals with a different name with respect to the target one

--- hdltools/map_signals.py
+++ hdltools/map_signals.py
@@ -14,17 +14,110 @@ class MapList(dict):
 
 	def __init__(self, elements_list, mode = "auto", *args, **kwargs):
 
+		self.elements_list = elements_list
+
 		if mode == "auto":
 		
-			index = 0
+			for key in self.elements_list:
 
-			for key in elements_list:
+				name = self.elements_list[key].name
 
-				name = elements_list[key].name
+				self[name] = MapObj(name, name)
 
-				self[index] = MapObj(name, name)
-				index = index + 1
+		elif mode == "pos":
+
+			if len(args) != len(self.elements_list):
+				print("Error, wrong number of elements in"
+						" signals mapping\n")
+				exit(-1)
+
+			else:
+
+				for source, target in zip(args, self.elements_list):
+
+					if type(source) == str:
+						source_name = source
+
+					elif hasattr(source, "name"):
+						source_name = source.name
+
+					else:
+						print("Error, wrong source in"
+							" signal mapping\n")
+
+					target_name = self.elements_list[target].name
+
+					self[target_name] = MapObj(target_name,
+							source_name)
+
+		elif mode == "key":
+
+			if len(kwargs) != len(self.elements_list):
+				print("Error, wrong number of elements in"
+						" signals mapping\n")
+				exit(-1)
+
 			
+			for target in self.elements_list:
+
+				if self.elements_list[target].name in kwargs:
+
+					if type(kwargs[target]) == str:
+						source_name = kwargs[target]
+
+					elif hasattr(kwargs[target], "name"):
+						source_name = \
+							kwargs[target].name
+
+					else:
+						print("Error, wrong source in"
+							" signal mapping\n")
+
+					target_name = self.elements_list[target].name
+
+					self[target_name] = MapObj(target_name,
+							source_name)
+
+				else:
+					print("Error, signal not mapped\n")
+					exit(-1)
+
+
+		elif mode == "no":
+			pass
+
+		else:
+			print("Wrong instance mode\n")
+			exit(-1)
+
+
+	def add(self, target_name, source):
+
+		present = False
+
+		for target in self.elements_list:
+			if target_name == self.elements_list[target].name:
+				present = True
+
+		if present:
+
+			if type(source) == str:
+				self[target_name] = MapObj(target_name, source)
+
+			elif hasattr(source, "name"):
+				self[target_name] = MapObj(target_name, source.name)
+
+			else:
+				print("Error, wrong source in signal mapping\n")
+				exit(-1)
+
+
+		else:
+			print("Signal not present in component\n")
+
+
+
+
 
 	def code(self, indent_level = 0):
 		return VHDLenum(self, indent_level)
